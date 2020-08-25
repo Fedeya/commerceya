4dfcc734e3596ce1b3a65d1c08a4de370fa344f2
Fedeya <elfedeminaya@gmail.com>
2020-08-24 22:46:36 -0300

    first commit

---
+++ packages/api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c19916c7-cd8b-43ce-811a-4a29721a59e9'
+};
---
+++ packages/web/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <Button>Hola Mundo</Button>
+    </>
+  );
+};
+
+export default Home;
